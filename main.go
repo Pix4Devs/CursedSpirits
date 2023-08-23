e8d2439a8db0e8ec2609043c82a08525823d94bc
Efdal <00312802@rijnijssel.nl>
2023-08-23 12:48:53 +0200

    fix

--- main.go
+++ main.go
@@ -3,17 +3,8 @@ package main
 import (
 	"Pix4Devs/CursedSpirits/cmd"
 	"Pix4Devs/CursedSpirits/fancy"
-	"flag"
 	"fmt"
 	"log"
-)
-
-var (
-	TARGET      = flag.String("url", "", "Target URL. Examples: https://github.com or http://google.com")
-	CONCURRENCY = flag.Int("concurrency", 2000, "Defines concurrency across requests")
-	DURATION    = flag.Int("duration", 300, "Flood duration in seconds")
-	PROXY_TYPE = flag.String("protocol", "socks4", "Proxy protocol, can be one of: socks4 or socks5")
-	VALID_PROTOCOLS = []string{"socks4","socks5"}
 )
 
 func main() {
